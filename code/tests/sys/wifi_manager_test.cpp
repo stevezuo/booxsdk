7793f92cb79a7b877e1f1c9b82308a1ab4976b9e
john <john@onyx-international.com>
2010-08-31 22:35:03 +0800

    Add wifi manager test.

---
+++ code/tests/sys/wifi_manager_test.cpp
@@ -0,0 +1,14 @@
+#include <QtGui/QtGui>
+#include <cassert>
+
+#include "onyx/base/qt_support.h"
+#include "onyx/sys/sys_status.h"
+#include "onyx/sys/wpa_connection_manager.h"
+
+using namespace sys;
+
+int main(int argc, char * argv[])
+{
+    return 0;
+}
+

d3a58da312bdc21d631e94829ffdddeb47074696
john <john@onyx-international.com>
2010-11-13 19:19:34 +0800

    add platform utils.

---
+++ code/include/onyx/sys/platform.h
@@ -0,0 +1,17 @@
+// Authors: John
+
+/// Public header of the system configuration library.
+
+#ifndef ONYX_SYSTEM_PLATFORM_H__
+#define ONYX_SYSTEM_PLATFORM_H__
+
+namespace sys
+{
+
+QString platform();
+
+int defaultRotation();
+
+}
+
+#endif  // ONYX_SYSTEM_PLATFORM_H__
---
+++ code/src/sys/platform.cpp
@@ -0,0 +1,32 @@
+
+#include "platform.h"
+
+namespace sys
+{
+
+QString platform()
+{
+    static QString p;
+    if (p.isEmpty())
+    {
+        p = qgetenv("PLATFORM");
+        if (p.isEmpty())
+        {
+            p = "freescale";
+        }
+    }
+    return p;
+}
+
+
+int defaultRotation()
+{
+    static int rotation = -1;
+    if (rotation < 0)
+    {
+        rotation = qgetenv("DEFAULT_ROTATION").toInt();
+    }
+    return rotation;
+}
+
+}    // namespace sys

d74960c2e8ea0d6e1471f1e5afde049c53ff3349
john <john@onyx-international.com>
2010-12-15 22:48:12 +0800

    1. Adjusted sound interface. 2. Add system env to enable/disable tts to open sound device automatically. By default, tts will automatically open the device.

--- code/src/sound/sound.cpp
+++ code/src/sound/sound.cpp
@@ -21,7 +21,7 @@ static void loadKernelModule()
     QStringList module;
     module << sys::soundModule();
     loader.start("modprobe", module);
-    const int TIME = 2000;
+    const int TIME = 5000;
     if (loader.waitForStarted(TIME))
     {
         loader.waitForFinished(TIME);
@@ -34,13 +34,16 @@ static void loadKernelModule()
 }
 
 
-Sound::Sound(const char *dev)
+Sound::Sound(bool o, const char *dev)
 : device_(-1)
 , enable_(true)
 , bps_(8)
 , channels_(0)
 {
-    open(dev);
+    if (o)
+    {
+        open(dev);
+    }
 }
 
 Sound::~Sound()
@@ -51,6 +54,7 @@ Sound::~Sound()
 bool Sound::open(const char *device_name)
 {
 #ifndef _WINDOWS
+    qDebug("open sound device.");
     device_ = ::open(device_name, O_WRONLY);
     if (device_ == -1)
     {

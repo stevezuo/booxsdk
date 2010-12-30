55abab44aabf2d1968f8bc3acd92c63cfc1c8593
Richard <richard@onyx-international.com>
2010-12-30 14:25:37 +0800

    fix bug

--- code/src/wireless/dialup_dialog.cpp
+++ code/src/wireless/dialup_dialog.cpp
@@ -181,8 +181,8 @@ int  DialUpDialog::popup(bool show_profile)
 
     // connect to default network.
     for(int i = 0; i < APNS_COUNT; ++i)
-    {
-        if(qgetenv("CONNECT_TO_DEFAULT_APN").toInt() == 1)
+    {   
+        if(qgetenv("CONNECT_TO_DEFAULT_APN").toInt() > 0)
         {
             if (sys_.isPowerSwitchOn())
             {

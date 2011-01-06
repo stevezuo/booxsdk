518ec6a61b2383bd997e3a9e536ce18278aeb04c
steve <steve@onyx-international.com>
2011-01-06 18:28:37 +0800

    Update for openning epub document on windows.

--- code/src/sys/sys_status.cpp
+++ code/src/sys/sys_status.cpp
@@ -22,6 +22,7 @@ static const QString TETHERED_DEVICE = "tethered";
 static const QString DEVICE_NAME = "OnyxBoox";
 static const QString ROOT_FOLDER = "/";
 static const QString DRM_CONTENT_DIR = "media/flash/DRM Contents";
+static const QString RESOURCE_FOLDER = "media/flash/resources/";
 static const QString DEVICE_FILE     = "media/flash/.adobe-digital-editions/device.xml";
 static const QString ACTIVATION_FILE = "media/flash/.adobe-digital-editions/activation.xml";
 
@@ -56,6 +57,20 @@ static void putDocumentFolder()
     QDir dir(path);
     qputenv("ADOBE_DE_DOC_FOLDER", dir.absoluteFilePath(DRM_CONTENT_DIR).toAscii());
     qDebug("ADOBE_DE_DOC_FOLDER : %s", qgetenv("ADOBE_DE_DOC_FOLDER").constData());
+}
+
+static void putResourceFolder()
+{
+    // set the folder of Digital Edition
+    QString path;
+#ifdef Q_WS_QWS
+    path = ROOT_FOLDER;
+#else
+    path = QDir::home().path();
+#endif
+    QDir dir(path);
+    qputenv("DEFAULT_FONT", dir.absoluteFilePath(RESOURCE_FOLDER).toAscii());
+    qDebug("DEFAULT_FONT : %s", qgetenv("DEFAULT_FONT").constData());
 }
 
 static void putDeviceName()
@@ -1389,6 +1404,9 @@ void SysStatus::addDRMEnvironment()
     putDocumentFolder();
     putDeviceName();
     putDeviceFile();
+#ifndef BUILD_FOR_ARM
+    putResourceFolder();
+#endif
 }
 
 // TODO, implement in system manager later.
